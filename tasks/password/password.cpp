9da24d763b1130a73037ce5a974ae1161ed58158
Artem Efimov <efimov12012004@mail.ru>
2023-01-20 17:29:12 +0300

    const names

--- tasks/password/password.cpp
+++ tasks/password/password.cpp
@@ -2,19 +2,22 @@
 
 #include <iostream>
 
-constexpr int cnst_126 = 126;
-constexpr int cnst_33 = 33;
+constexpr int Cnst_126 = 126;
+constexpr int Cnst_33 = 33;
+constexpr int Cnst_8 = 8;
+constexpr int Cnst_14 = 14;
+constexpr int Cnst_3 = 3;
 
 bool ValidatePassword(const std::string& password) {
     int big_letters = 0;
     int small_letters = 0;
     int numbers = 0;
     int other = 0;
-    if (password.size() < 8 || password.size() > 14) {
+    if (password.size() < Cnst_8 || password.size() > Cnst_14) {
         return false;
     }
     for (size_t i = 0; i < password.size(); ++i) {
-        if (int(password[i]) < cnst_33 || int(password[i]) > cnst_126) {
+        if (int(password[i]) < Cnst_33 || int(password[i]) > Cnst_126) {
             return false;
         }
         if (std::isupper(static_cast<unsigned char>(password[i]))) {
@@ -31,7 +34,7 @@ bool ValidatePassword(const std::string& password) {
         }
         other = 1;
     }
-    if (big_letters + small_letters + numbers + other < 3) {
+    if (big_letters + small_letters + numbers + other < Cnst_3) {
         return false;
     }
     return true;
