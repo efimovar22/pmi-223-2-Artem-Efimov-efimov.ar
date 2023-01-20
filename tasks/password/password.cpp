2ac85053f1e53c6f6f1a2912e8ce0f1bd42948bf
Artem Efimov <efimov12012004@mail.ru>
2023-01-20 17:33:40 +0300

    const names

--- tasks/password/password.cpp
+++ tasks/password/password.cpp
@@ -2,22 +2,22 @@
 
 #include <iostream>
 
-constexpr int Cnst_126 = 126;
-constexpr int Cnst_33 = 33;
-constexpr int Cnst_8 = 8;
-constexpr int Cnst_14 = 14;
-constexpr int Cnst_3 = 3;
+constexpr int Cnst126 = 126;
+constexpr int Cnst33 = 33;
+constexpr int Cnst8 = 8;
+constexpr int Cnst14 = 14;
+constexpr int Cnst3 = 3;
 
 bool ValidatePassword(const std::string& password) {
     int big_letters = 0;
     int small_letters = 0;
     int numbers = 0;
     int other = 0;
-    if (password.size() < Cnst_8 || password.size() > Cnst_14) {
+    if (password.size() < Cnst8 || password.size() > Cnst14) {
         return false;
     }
     for (size_t i = 0; i < password.size(); ++i) {
-        if (int(password[i]) < Cnst_33 || int(password[i]) > Cnst_126) {
+        if (int(password[i]) < Cnst33 || int(password[i]) > Cnst126) {
             return false;
         }
         if (std::isupper(static_cast<unsigned char>(password[i]))) {
@@ -34,7 +34,7 @@ bool ValidatePassword(const std::string& password) {
         }
         other = 1;
     }
-    if (big_letters + small_letters + numbers + other < Cnst_3) {
+    if (big_letters + small_letters + numbers + other < Cnst3) {
         return false;
     }
     return true;
