956b9e59fface63463588013b0f29d82e01b0461
Artem Efimov <efimov12012004@mail.ru>
2023-01-23 07:15:40 +0300

    changed type of const, names of const

--- tasks/password/password.cpp
+++ tasks/password/password.cpp
@@ -2,39 +2,39 @@
 
 #include <iostream>
 
-constexpr int Cnst126 = 126;
-constexpr int Cnst33 = 33;
-constexpr int Cnst8 = 8;
-constexpr int Cnst14 = 14;
-constexpr int Cnst3 = 3;
+const char MAX_POS_VAL = '~';
+const char MIN_POS_VAL = '!';
+const char MIN_NUM_SYMB = 8;
+const char MAX_NUM_SYMB = 14;
+const char DIFFERENT_TYPES = 3;
 
 bool ValidatePassword(const std::string& password) {
-    int big_letters = 0;
-    int small_letters = 0;
-    int numbers = 0;
-    int other = 0;
-    if (password.size() < Cnst8 || password.size() > Cnst14) {
+    bool has_big_letters = 0;
+    bool has_small_letters = 0;
+    bool has_numbers = 0;
+    bool has_other = 0;
+    if (password.size() < MIN_NUM_SYMB || password.size() > MAX_NUM_SYMB) {
         return false;
     }
     for (size_t i = 0; i < password.size(); ++i) {
-        if (int(password[i]) < Cnst33 || int(password[i]) > Cnst126) {
+        if (password[i] < MIN_POS_VAL || password[i] > MAX_POS_VAL) {
             return false;
         }
-        if (std::isupper(static_cast<unsigned char>(password[i]))) {
-            big_letters = 1;
+        if (std::isupper(password[i])) {
+            has_big_letters = 1;
             continue;
         }
-        if (std::islower(static_cast<unsigned char>(password[i]))) {
-            small_letters = 1;
+        if (std::islower(password[i])) {
+            has_small_letters = 1;
             continue;
         }
-        if (std::isdigit(static_cast<unsigned char>(password[i]))) {
-            numbers = 1;
+        if (std::isdigit(password[i])) {
+            has_numbers = 1;
             continue;
         }
-        other = 1;
+        has_other = 1;
     }
-    if (big_letters + small_letters + numbers + other < Cnst3) {
+    if (has_big_letters + has_small_letters + has_numbers + has_other < DIFFERENT_TYPES) {
         return false;
     }
     return true;
