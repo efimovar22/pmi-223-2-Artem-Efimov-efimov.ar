2cde278ce332e5edade0acc2d68dc3272c0a8b41
Artem Efimov <efimov12012004@mail.ru>
2023-02-10 13:15:34 +0300

    1st try

--- tasks/tests_checking/tests_checking.cpp
+++ tasks/tests_checking/tests_checking.cpp
@@ -1,6 +1,19 @@
 #include "tests_checking.h"
+#include <deque>
 
 std::vector<std::string> StudentsOrder(const std::vector<StudentAction>& student_actions,
                                        const std::vector<size_t>& queries) {
-    return {};
+    std::deque<std::string> d = {};
+    for (size_t i = 0; i < student_actions.size(); ++i) {
+        if (student_actions[i].side == Side::Top) {
+            d.push_front(student_actions[i].name);
+        } else {
+            d.push_back(student_actions[i].name);
+        }
+    }
+    std::vector<std::string> ans = {};
+    for (size_t i = 0; i < queries.size(); ++i) {
+        ans.push_back(d[queries[i]]);
+    }
+    return ans;
 }
