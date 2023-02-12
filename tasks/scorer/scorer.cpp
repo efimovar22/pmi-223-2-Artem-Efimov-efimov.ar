e4cd8e95a59ded32cac29578e337ba98dda1badd
Artem Efimov <efimov12012004@mail.ru>
2023-02-12 16:00:32 +0300

    delete if

--- tasks/scorer/scorer.cpp
+++ tasks/scorer/scorer.cpp
@@ -7,7 +7,7 @@ bool TimeComparator(const Event* event_1, const Event* event_2) {
 
 ScoreTable GetScoredStudents(const Events& events, time_t score_time) {
     ScoreTable ans = {};
-    std::map<std::string, std::map<std::string, std::pair<int, int>>> check_tasks;
+    std::map<std::string, std::map<std::string, std::pair<bool, bool>>> check_tasks;
     std::vector<const Event*> sorted_events;
     for (auto& i : events) {
         sorted_events.push_back(&i);
@@ -17,27 +17,19 @@ ScoreTable GetScoredStudents(const Events& events, time_t score_time) {
         if (i->time > score_time) {
             break;
         }
-        if (not check_tasks.contains(i->student_name)) {
-            check_tasks[i->student_name][i->task_name].first = 0;
-            check_tasks[i->student_name][i->task_name].second = 0;
-        }
-        if (not check_tasks[i->student_name].contains(i->task_name)) {
-            check_tasks[i->student_name][i->task_name].first = 0;
-            check_tasks[i->student_name][i->task_name].second = 0;
-        }
         if (i->event_type == EventType::CheckSuccess) {
-            check_tasks[i->student_name][i->task_name].first = 1;
+            check_tasks[i->student_name][i->task_name].first = true;
         } else if (i->event_type == EventType::MergeRequestOpen) {
-            check_tasks[i->student_name][i->task_name].second = 1;
+            check_tasks[i->student_name][i->task_name].second = true;
         } else if (i->event_type == EventType::MergeRequestClosed) {
-            check_tasks[i->student_name][i->task_name].second = 0;
+            check_tasks[i->student_name][i->task_name].second = false;
         } else if (i->event_type == EventType::CheckFailed) {
-            check_tasks[i->student_name][i->task_name].second = 0;
+            check_tasks[i->student_name][i->task_name].second = false;
         }
     }
     for (const auto& [i, j] : check_tasks) {
         for (const auto& [a, b] : j) {
-            if (b.first == 1 && b.second == 0) {
+            if (b.first && not b.second) {
                 ans[i].insert(a);
             }
         }
