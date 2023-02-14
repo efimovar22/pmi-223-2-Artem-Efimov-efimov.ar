5504f4368e11f76e2b8fb64e2ab5ab1075fd57ca
Artem Efimov <efimov12012004@mail.ru>
2023-02-14 14:16:44 +0300

    ctrl + alt + l

--- tasks/admission/admission.cpp
+++ tasks/admission/admission.cpp
@@ -1,19 +1,16 @@
 #include "admission.h"
 
 bool PointsComparator(const Applicant* applicant_1, const Applicant* applicant_2) {
-    return std::tie(applicant_1->points, applicant_2->student.birth_date.year,
-                    applicant_2->student.birth_date.month, applicant_2->student.birth_date.day,
-                    applicant_1->student.name) >
-           std::tie(applicant_2->points, applicant_1->student.birth_date.year,
-                    applicant_1->student.birth_date.month, applicant_1->student.birth_date.day,
-                    applicant_2->student.name);
+    return std::tie(applicant_1->points, applicant_2->student.birth_date.year, applicant_2->student.birth_date.month,
+                    applicant_2->student.birth_date.day, applicant_1->student.name) >
+           std::tie(applicant_2->points, applicant_1->student.birth_date.year, applicant_1->student.birth_date.month,
+                    applicant_1->student.birth_date.day, applicant_2->student.name);
 }
 
-bool NameComparator (const Student* student_1, const Student* student_2) {
-    return std::tie(student_1->name, student_1->birth_date.year,
-                    student_1->birth_date.month, student_1->birth_date.day) <
-           std::tie(student_2->name, student_2->birth_date.year,
-                    student_2->birth_date.month, student_2->birth_date.day);
+bool NameComparator(const Student* student_1, const Student* student_2) {
+    return std::tie(student_1->name, student_1->birth_date.year, student_1->birth_date.month,
+                    student_1->birth_date.day) < std::tie(student_2->name, student_2->birth_date.year,
+                                                          student_2->birth_date.month, student_2->birth_date.day);
 }
 
 AdmissionTable FillUniversities(const std::vector<University>& universities, const std::vector<Applicant>& applicants) {
