cbe55ccb96ac11e81a05a3c5ce53bd013c9ddc12
Artem Efimov <efimov12012004@mail.ru>
2023-03-19 13:47:12 +0300

    1st try

--- tasks/zip2/zip.h
+++ tasks/zip2/zip.h
@@ -2,6 +2,50 @@
 
 #include <iterator>
 
-template <typename Sequence1, typename Sequence2>
-auto Zip(const Sequence1& sequence1, const Sequence2& sequence2) {
+template <typename T1, typename T2>
+class ZipIterator {
+public:
+    ZipIterator(T1 a, T2 b) {
+        iterator1_ = a;
+        iterator2_ = b;
+    };
+    bool operator!=(const ZipIterator& other) {
+        return iterator1_ != other.iterator1_ && iterator2_ != other.iterator2_;
+    }
+    auto operator++() {
+        return std::make_pair(++iterator1_, ++iterator2_);
+    }
+    auto operator*() {
+        return std::pair{*iterator1_, *iterator2_};
+    }
+
+private:
+    T1 iterator1_;
+    T2 iterator2_;
+};
+
+template <typename T1, typename T2>
+class ZipObject {
+public:
+    ZipObject(T1 beg1, T1 end1, T2 beg2, T2 end2) : beg1_(beg1), end1_(end1), beg2_(beg2), end2_(end2) {
+    }
+
+    auto begin() {
+        return ZipIterator(beg1_, beg2_);
+    }
+
+    auto end() {
+        return ZipIterator(end1_, end2_);
+    }
+
+private:
+    T1 beg1_;
+    T1 end1_;
+    T2 beg2_;
+    T2 end2_;
+};
+
+template <typename Obj1, typename Obj2>
+auto Zip(const Obj1& obj1, const Obj2& obj2) {
+    return ZipObject(std::begin(obj1), std::end(obj1), std::begin(obj2), std::end(obj2));
 }
