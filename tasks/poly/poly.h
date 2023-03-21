d9d81f04f0c4bb8a2d5cce1b5a3a6e6e2e783b8a
Artem Efimov <efimov12012004@mail.ru>
2023-03-21 19:22:15 +0300

    1st try

--- tasks/poly/poly.h
+++ tasks/poly/poly.h
@@ -1,4 +1,31 @@
 #pragma once
 
+#include <initializer_list>
+#include <map>
+#include <iostream>
+#include <vector>
+#include <iterator>
+
 class Poly {
+public:
+    void ZeroCoef();
+    Poly() = default;
+    Poly(std::initializer_list<std::pair<int64_t, int64_t>> poly);
+    Poly(std::initializer_list<int64_t> poly);
+    Poly(const Poly& other) = default;
+    Poly operator+=(const Poly& other);
+    Poly operator-=(const Poly& other);
+    Poly operator*=(const Poly& other);
+    Poly operator+(const Poly& other);
+    Poly operator-(const Poly& other);
+    Poly operator*(const Poly& other);
+    Poly operator-() const;
+    bool operator==(const Poly& other) const;
+    bool operator!=(const Poly& other) const;
+    Poly operator=(const Poly& other);
+    int64_t operator()(int64_t x) const;
+
+private:
+    friend std::ostream& operator<<(std::ostream& out, const Poly&);
+    std::map<int64_t, int64_t> coeffs_;
 };
