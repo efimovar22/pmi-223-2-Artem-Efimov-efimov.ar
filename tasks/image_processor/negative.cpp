742747b80614b838b28c450373550cdb5e536439
Artem Efimov <efimov12012004@mail.ru>
2023-03-26 22:00:29 +0300

    "change b and r"

--- tasks/image_processor/negative.cpp
+++ tasks/image_processor/negative.cpp
@@ -6,9 +6,9 @@ Image Negative::Apply(const Image& image) const {
     for (size_t i = 0; i < image.Height(); ++i) {
         std::vector<Color> vec(image.Width());
         for (size_t j = 0; j < image.Width(); ++j) {
-            vec[j] = {static_cast<double>(BIT_BMP - image.GetColor(i, j).Blue),
+            vec[j] = {static_cast<double>(BIT_BMP - image.GetColor(i, j).Red),
                       static_cast<double>(BIT_BMP - image.GetColor(i, j).Green),
-                      static_cast<double>(BIT_BMP - image.GetColor(i, j).Red)};
+                      static_cast<double>(BIT_BMP - image.GetColor(i, j).Blue)};
         }
         ans.push_back(vec);
     }
