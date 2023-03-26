0e6948f971893a7d6ea9406143be434941f70d1c
Artem Efimov <efimov12012004@mail.ru>
2023-03-26 23:03:27 +0300

    "changed filter"

--- tasks/image_processor/sharpening.cpp
+++ tasks/image_processor/sharpening.cpp
@@ -7,21 +7,21 @@ Image Sharpening::Apply(const Image &image) const {
     for (size_t i = 0; i < image.Height(); ++i) {
         std::vector<Color> vec(image.Width());
         for (size_t j = 0; j < image.Width(); ++j) {
-            vec[j].Red = -(image.GetColorsVector()[std::max(i, static_cast<size_t>(1)) - 1][j].Red +
+            vec[j].Red = std::max(0.0, std::min(1.0, -(image.GetColorsVector()[std::max(i, static_cast<size_t>(1)) - 1][j].Red +
                            image.GetColorsVector()[std::min(i + 1, image.Height() - 1)][j].Red +
                            image.GetColorsVector()[i][std::max(j, static_cast<size_t>(1)) - 1].Red +
                            image.GetColorsVector()[i][std::min(j + 1, image.Width() - 1)].Red) +
-                         five * image.GetColorsVector()[i][j].Red;
-            vec[j].Green = -(image.GetColorsVector()[std::max(i, static_cast<size_t>(1)) - 1][j].Green +
+                         five * image.GetColorsVector()[i][j].Red));
+            vec[j].Green = std::max(0.0, std::min(1.0, -(image.GetColorsVector()[std::max(i, static_cast<size_t>(1)) - 1][j].Green +
                              image.GetColorsVector()[std::min(i + 1, image.Height() - 1)][j].Green +
                              image.GetColorsVector()[i][std::max(j, static_cast<size_t>(1)) - 1].Green +
                              image.GetColorsVector()[i][std::min(j + 1, image.Width() - 1)].Green) +
-                           five * image.GetColorsVector()[i][j].Green;
-            vec[j].Blue = -(image.GetColorsVector()[std::max(i, static_cast<size_t>(1)) - 1][j].Blue +
+                           five * image.GetColorsVector()[i][j].Green));
+            vec[j].Blue = std::max(0.0, std::min(1.0, -(image.GetColorsVector()[std::max(i, static_cast<size_t>(1)) - 1][j].Blue +
                             image.GetColorsVector()[std::min(i + 1, image.Height() - 1)][j].Blue +
                             image.GetColorsVector()[i][std::max(j, static_cast<size_t>(1)) - 1].Blue +
                             image.GetColorsVector()[i][std::min(j + 1, image.Width() - 1)].Blue) +
-                          five * image.GetColorsVector()[i][j].Blue;
+                          five * image.GetColorsVector()[i][j].Blue));
         }
         ans.push_back(vec);
     }
